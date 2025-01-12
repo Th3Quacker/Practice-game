7d1acb5d69d043fc577ef66699b0c2d68d457385
Charlie <180675239+Th3Quacker@users.noreply.github.com>
2025-01-12 02:36:20 -0700

    Create index.js

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("poto");
+const ctx = canvas.getContext("2d");
+var CANVAS_WIDTH = canvas.width = window.innerWidth;
+var CANVAS_HEIGHT = canvas.height = window.innerHeight;
+var gameStarted = false;
+var startButton = {
+  x: 100,
+  y: 100,
+  width: 100,
+  height: 100,
+}
+window.addEventListener("click", e =>{
+  if (!gameStarted) {
+    if (e.x > startButton.x && e.x < startButton.x + startButton.width && e.y > startButton.y && e.y < startButton.y + startButton.height) {
+      gameStarted = true
+    }
+  }
+})
+function ani(){
+  if(gameStarted){
+    ctx
+  }else{
+    
+  }
+  requestAnimationFrame(ani);
+}
+ani();
